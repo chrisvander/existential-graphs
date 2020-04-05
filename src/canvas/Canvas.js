160f4fe8d5d229be92a8725db89f5b6078f15357
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-04-05 19:06:38 -0400

    removed canvas

--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import InfiniteCanvas from 'infinite-canvas';
-import { Stage, Shape, Ticker, Bitmap, Text } from "@createjs/easeljs";
 
 class Canvas extends React.Component {
   constructor(props) {
@@ -8,8 +6,6 @@ class Canvas extends React.Component {
 
     console.log(this.props.proof)
 
-    this.canvas = React.createRef();
-    this.deviceScale = window.devicePixelRatio
     let { premises, conclusion, steps } = this.props.proof;
     this.state = {
       proof: {
@@ -21,28 +17,8 @@ class Canvas extends React.Component {
     };
   }
 
-  resize(canvas) {
-    let ctx = canvas.getContext('2d');
-    let scale = this.deviceScale;
-    canvas.width = window.innerWidth * scale;
-    canvas.height = window.innerHeight * scale;
-    canvas.style.width = window.innerWidth + "px";
-    canvas.style.height = window.innerHeight + "px";
-    ctx.scale(scale, scale);
-    return canvas;
-  }
-
   componentDidMount() {
-    // initialize stage
-    this.stage = new Stage(this.canvas.current);
-    window.addEventListener('resize', () => {
-      this.canvas.current = this.resize(this.canvas.current);
-    });
-    Ticker.addEventListener("tick", (event) => {
-      this.stage.update();
-    });
-    this.resize(this.canvas.current);
-
+    
     // if there are no existing steps, init first step
     let { steps } = this.state;
     if (steps.length === 0) {
@@ -60,10 +36,7 @@ class Canvas extends React.Component {
 
   render() {
     return (
-      <canvas 
-        onDrop={this.onDrop}
-        onDragOver={(e)=>e.preventDefault()}
-        ref={this.canvas} />
+      <canvas ref={this.canvas} />
     );
   }
 }
